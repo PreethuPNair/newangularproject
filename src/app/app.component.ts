ebba0c6f2b922caabf5e85418d9e28983bff556c
PreethuPNair <93589470+PreethuPNair@users.noreply.github.com>
2024-03-14 22:23:33 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,39 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angproj';
+
+  ngOnInit(): void {
+    
+    this.example();
+  }
+
+  example() {
+
+
+    var x = 10;
+
+    if (true) {
+      var x = 20; // This is allowed
+      console.log(x); // Output: 20
+    }
+    console.log(x); // Output: 20
+
+
+    let y = 10;
+    if (true) {
+      let y = 20; // This is not allowed
+      console.log(y); // Output: 20
+    }
+    console.log(y); // Output: 10
+
+  
+    const z = 10;
+    if (true) {
+      const z = 20; // This is not allowed
+      console.log(z); // Output: 10
+    }
+    console.log(z); // Output: 10
+  }
+
+
 }
